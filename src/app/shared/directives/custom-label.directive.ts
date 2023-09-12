6b24b15f9ba12287b9927b4decec7683318b9d42
Facundo <wehrlifacundo9@gmail.com>
2023-09-12 12:04:51 -0300

    feat: change tag message

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,17 +1,24 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
-  selector: '[customLabel]'
+  selector: '[customLabel]',
 })
-export class CustomLabelDirective implements OnInit{
-
+export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
-  @Input() set color(value: string){
+  @Input() set color(value: string) {
     // console.log({color: value})
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    // console.log(value)
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -20,15 +27,42 @@ export class CustomLabelDirective implements OnInit{
     this.htmlElement = el;
 
     // this.htmlElement.nativeElement.innerHTML = 'Hola Mundo'
-   }
+  }
   ngOnInit(): void {
     // console.log('Directiva - NgOnInit')
     this.setStyle();
   }
 
-  setStyle():void{
-    if(!this.htmlElement) return;
+  setStyle(): void {
+    if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
   }
 
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    // console.log(errors)
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerText = `Mnímimo ${current}/${min} caracteres`;
+      return;
+    }
+
+    if(errors.includes('email')){
+      this.htmlElement.nativeElement.innerText = 'Formato email erróneo';
+
+    }
+
+  }
 }
